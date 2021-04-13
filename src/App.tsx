214fee56983aefc34a94f8bbbdabaa632c4d6cbb
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2021-04-13 12:24:26 -0300

    Base App setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import './App.scss';
+import Hangman from './components/Hangman/Hangman';
+import Letters from './components/Letters/Letters';
+import Time from './components/Time/Time';
+import Wordbox from './components/Wordbox/Wordbox';
+import GameContext from './contexts/gameContext';
+import { getRandomWord } from './words';
 
 function App() {
+  const [errors, setErrors] = useState(0);
+  const [word, setWord] = useState(getRandomWord());
+  const [selectedLetters, setSelectedLetters] = useState<string[]>([]);
+  const [history, setHistory] = useState(false);
+
+  const timeIsOut = () => setWord(getRandomWord());
+
+  useEffect(() => {
+    setErrors(0);
+    setSelectedLetters([]);
+    if (!word) {
+      setWord(getRandomWord());
+    }
+  }, [word]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GameContext.Provider
+      value={{
+        errors,
+        setErrors,
+        selectedLetters,
+        setSelectedLetters,
+        timeIsOut,
+        word
+      }}>
+      <div className='App'>
+        <h1>The Hangman Game</h1>
+        <Hangman />
+        {errors === 6 ? <h3>You lost!</h3> : null}
+        <div className='info'>
+          <Time />
+          <div className='history' onClick={() => setHistory(true)}>
+            <i className='fas fa-history' />
+            History
+          </div>
+          <div className='restart' onClick={() => setWord(getRandomWord())}>
+            <i className='fas fa-redo' />
+            New game
+          </div>
+        </div>
+        <Wordbox />
+        <Letters />
+      </div>
+    </GameContext.Provider>
   );
 }
 
