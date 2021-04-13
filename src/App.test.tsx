58ad1aad1f1dcbe3e01001b8272406a9447f5411
Rafael Lopes Schneider <rafael.rls70@gmail.com>
2021-04-13 13:21:51 -0300

    Firebase setup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+  let component;
+
+  beforeEach(() => {
+    component = shallow(<App />);
+  });
+
+  test('It should mount', () => {
+    expect(component.length).toBe(1);
+  });
 });
